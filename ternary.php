8c22cad25b5529acd086f8b570929180f8cc3526
Rialahmed0 <rashedahommod0@gmail.com>
2025-06-13 21:23:03 +0600

    ternary operator

---
+++ ternary.php
@@ -0,0 +1,13 @@
+<?php 
+$num = -1 ;
+
+$checkNumber = ($num > 0) ? 'this is positive number' : 'this is negative number ' ;
+
+// echo $checkNumber ;
+
+$age = 25 ;
+$checkAge = ($age >= 18) ? 'you are adult ' : 'you are minor ' ;
+// echo $checkAge
+
+
+?>
