1779fd80c48fb8cc99c9b36f5146dc1fa59faf6a
Rialahmed0 <rashedahommod0@gmail.com>
2025-06-13 21:15:10 +0600

    if else statement

---
+++ ifElse.php
@@ -0,0 +1,23 @@
+<?php
+$name = 'rial';
+
+$number = 60 ;
+if ($number > 80) {
+    echo 'you got a+' , '<br>';
+}
+elseif ($number > 69 && $number < 80) {
+    echo ' you got a' , '<br>';
+}
+else{
+    echo 'you are fail ' , '<br>';
+}
+
+$temp = 20 ;
+if ($temp > 20) {
+    echo 'today is hot ' , '<br>';
+}
+else{
+    echo 'today is cold' , '<br>';
+}
+
+?>
